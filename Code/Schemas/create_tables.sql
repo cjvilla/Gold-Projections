6a4768db336980012a46e342ea15e7c75c31e7c3
cjvilla <CelineVillareal2020@u.northwestern.edu>
2021-06-09 01:30:04 -0500

    added country schemas

---
+++ Code/Schemas/create_tables.sql
@@ -0,0 +1,23 @@
+create table gold_prices (
+	Name date,
+	USA float8,
+	Euro float8,
+	Japan float8,
+	UK float8,
+	Canada float8,
+	Swizerland float8,
+	India float8,
+	China float8,
+	Turkey float8,
+	SB float8,
+	Indonesia float8,
+	UAE float8,
+	Thailand float8,
+	Vietnam float8,
+	Egypt float8,
+	SK float8,
+	Russia float8,
+	SA float8,
+	Australia float8
+);
+
