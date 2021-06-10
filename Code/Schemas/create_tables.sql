3dff4cc269e821d461eca3fa5fe9bb8c46156837
cjvilla <CelineVillareal2020@u.northwestern.edu>
2021-06-09 21:45:47 -0500

    upload final schemas

--- Code/Schemas/create_tables.sql
+++ Code/Schemas/create_tables.sql
@@ -1,51 +1,41 @@
-create table gold_prices (
-	date_recorded date,
-	USA float8,
-	Euro float8,
-	Japan float8,
-	UK float8,
-	Canada float8,
-	Swizerland float8,
-	India float8,
-	China float8,
-	Turkey float8,
-	SB float8,
-	Indonesia float8,
-	UAE float8,
-	Thailand float8,
-	Vietnam float8,
-	Egypt float8,
-	SK float8,
-	Russia float8,
-	SA float8,
-	Australia float8
+create final_inflation_production (
+	Year integer,
+	Month integer,
+	Inflation_Value double precision
 );
 
-create table country_gold_production (
-	Name text,
-	Code text,
-	date_recorded date,
-	gold_production float8
+create table final_us_dollar_index (
+	Year integer,
+	Month integer,
+	Date date,
+	Value double precision
 );
 
-create table inflation_rates (
-	date_recorded date,
-	JAN float8,
-	FEB float8,
-	MAR float8,
-	APR float8,
-	MAY float8,
-	JUN float8,
-	JUL float8,
-	AUG float8,
-	SEP float8,
-	OCT float8,
-	NOV float8,
-	DEC float8,
-	AVE float8
+create table monthly_gold_prices_final (
+	Date date,
+	Year integer,
+	month integer,
+	us_dollar double precision,
+	euro double precision,
+	japanese_yen double precision
+	pound double precision,
+	canadian_dollar double precision,
+	swiss_franc double precision,
+	indian_rupee double precision,
+	saudi_riyal double precision,
+	uae_dirham double precision,
+	thai_baht double precision,
+	korean_won double precision,
+	south_african_rand double precision,
+	australian_dollar double precision,
 );
 
-create table dollar_index (
-	date_recorded date,
-	value float8
-);
+create table gold_prediction_data
+	AS
+(select x."Year", x."Month", x."Inflation_Value", x."dollar_index", j."us_dollar", j."pound", j."indian_rupee", j."sauth_african_rand", j."australian_dollar"
+from gold_prediction_data x
+join monthly_gold_prices_final j
+on x."Year" = j."year" and x."Month" = j."month");
+
+
+
