a97c9fc17b85d26e85518456f8a810283ffbdfe3
emily-wuenstel <74880899+emily-wuenstel@users.noreply.github.com>
2021-06-12 10:12:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ class Data(db.Model):
 #################################################
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 ################################################
